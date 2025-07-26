8721f62c0689b785598e102be5b5819aa6c13df0
Christian <konfusingcrate1113@gmail.com>
2025-07-26 01:14:27 -0400

    changed sign in page layout

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,21 @@
-const SignInPage = () => {
+import { SignIn } from '@clerk/nextjs'
+
+export default function Page() {
   return (
-    <div>Sign in page</div>
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">
+            Welcome Back!
+          </h1>
+          <p className="text-base text-[#7E8CA0]">
+            Log in or Create account to get back to your dashboard!
+          </p>
+        </div>
+        <div className="flex item-center justify-center mt-8">
+          <SignIn />
+        </div>
+      </div>
+    </div>
   )
 }
-
-export default SignInPage
