bfb274f238627cb179d528416593b7f244465762
Christian <konfusingcrate1113@gmail.com>
2025-07-29 21:57:51 -0400

    added authors and books to route

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,27 +1,14 @@
-import { z } from 'zod'
-import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import authors from './authors'
+import books from './books'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app
-  .get(
-    '/hello',
-    clerkMiddleware(),
-    (c) => { // c is context
-      const auth = getAuth(c)
-      if (!auth?.userId) {
-        return c.json({ error: 'Unauthorized' })
-      }
-
-      return c.json({
-      message: 'Hello Next.js!',
-      })
-  })
+app.route('/authors', authors)
+app.route('/books', books)
 
 // route handlers
 export const GET = handle(app) 
